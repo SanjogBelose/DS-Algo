efa25bb0bb5a1beba4e426d0324550a27ee83112
SanjogBelose <s.belose2712@gmail.com>
2022-02-22 20:29:30 +0530

    Height.cpp updated

--- Array/demo.cpp
+++ Array/demo.cpp
@@ -1,3 +1,5 @@
+// This file is just for testing purpose
+
 #include<iostream>
 using namespace std;
 
@@ -5,6 +7,8 @@ int main() {
 
     int k = 1;
     int arr[] = {1,2,3,4,5,6,7,8};
+
+    cout << sizeof(arr)/sizeof(arr[0]);
 
     for(int i = 0; i < k - 1; i++){
         cout << arr[i] << "\n";
--- Array/height.cpp
+++ Array/height.cpp
@@ -1,52 +1,35 @@
-#include<iostream>
-#include<set>
+//Minimize the Heights II
+
 #include <bits/stdc++.h>
 using namespace std;
 
 int main(){
 
-    int arr[] = {2, 6, 3, 4, 7, 2, 10, 3, 2, 1};
+    int arr[] = {8, 1, 5, 4, 7, 5, 7, 9, 4, 6};
     int n = 10;
     int k = 5;
 
-    int z;
-    int max;
-    int min;
-    int answer;
+    sort(arr, arr + n);
 
-    set<int> v;
-    for(int i = 0; i < n; i++){
-        v.insert(arr[i]);
-    }
+    int minimum;
+    int maximum;
+    int diff;
+    int ans = arr[n-1] - arr[0];
 
-    z = v.size();
 
-     copy(v.begin(), v.end(), arr);
+    for(int i = 1; i < n; i++){
+        if(arr[i]>=k){
+            maximum = max(arr[n-1]-k, arr[i-1]+k);
+            minimum = min(arr[0]+k, arr[i]-k);
 
-    for(int i = 0; i < z; i++){
-        if(arr[i] % 2 == 1){
-            arr[i] += k;
-            cout << arr[i] << "\n";
+            diff = maximum - minimum;
+
+            ans = min(ans, diff);
         }
-        else if(arr[i] %2 == 0){
-            arr[i] -= k;
-            cout << arr[i] << "\n";
-        }
+        
     }
 
-    vector<int> vec;
-
-    for(int i = 0; i < z; i++){
-        vec.push_back(arr[i]);
-    }
-
-
-    max = *max_element(vec.begin(), vec.end());
-    min = *min_element(vec.begin(), vec.end());
-    answer = max - min;
-
-    cout << answer;
-    cout << "\n";
+    cout << ans;
 
     return 0;
 }
